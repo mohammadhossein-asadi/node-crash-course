9234181da11ac2cb72089dd14ea15475c76989b4
mohammadhossein <mohammadhosseinasadi.p@gmail.com>
2022-03-13 22:42:40 +0330

    commit

--- files.js
+++ files.js
@@ -1,16 +1,25 @@
 const fs = require("fs");
 
-// reading files
-fs.readFile("./docs/blog1.txt", (err, data) => {
-  if (err) {
-    console.log(err);
-  }
-  //* use toString() for convert Buffer data to string
-  console.log(data.toString());
+//# reading files
+//* recive 2 argument // path // callback function
+
+// fs.readFile("./docs/blog1.txt", (err, data) => {
+//   if (err) {
+//     console.log(err);
+//   }
+//   //* use toString() for convert Buffer data to string
+//   console.log(data.toString());
+// });
+
+// console.log("last line");
+
+//# writing files
+//* recive 3 argument // path // text // callback function
+
+fs.writeFile("./docs.blog1.txt", "hello, world!", () => {
+  console.log("File was written");
 });
 
-// writing files
+//# directories
 
-// directories
-
-// deleting files
+//# deleting files
