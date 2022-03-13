1fbc5937c0a52f000d1e8f9c728bad36806c8323
mohammadhossein <mohammadhosseinasadi.p@gmail.com>
2022-03-14 00:19:06 +0330

    added file

--- files.js
+++ files.js
@@ -16,13 +16,13 @@ const fs = require("fs");
 //# writing files
 //* recive 3 parameter // path // text // callback function
 
-// fs.writeFile("./docs.blog1.txt", "hello, world!", () => {
+// fs.writeFile("./docs/blog1.txt", "hello, world!", () => {
 //   console.log("File was written ;)");
 // });
 
-//* This part of the file does not exist and is created
+// //* This part of the file does not exist and is created
 
-// fs.writeFile("./docs.blog2.txt", "hello, friend!", () => {
+// fs.writeFile("./docs/blog2.txt", "hello, friend!", () => {
 //   console.log("File was written ;)");
 // });
 
@@ -32,13 +32,30 @@ const fs = require("fs");
 // * create directorie
 // * recive 2 parameter // path // callback function
 
-if (!fs.existsSync("./assets")) {
-  fs.mkdir("./assets", (err) => {
-    if (err) {
-      console.log(err);
-    }
-    console.log("folder created");
-  });
-}
+// if (!fs.existsSync("./assets")) {
+//   fs.mkdir("./assets", (err) => {
+//     if (err) {
+//       console.log(err);
+//     }
+//     console.log("folder created");
+//   });
+// } else {
+//   fs.rmdir("./assets", (err) => {
+//     if (err) {
+//       console.log(err);
+//     }
+//     console.log("folder deleted");
+//   });
+// }
 
 //# deleting files
+
+// if (fs.existsSync("./docs/deleteme.txt")) {
+//   // * unlink recive 2 parameter
+//   fs.unlink("./docs/deleteme.txt", (err) => {
+//     if (err) {
+//       console.log(err);
+//     }
+//     console.log("file deleted");
+//   });
+// }
