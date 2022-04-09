1596b56e226159bc741a5329d10e362700c0c2a7
mohammadhossein <mohammadhosseinasadi.p@gmail.com>
2022-04-09 11:36:57 +0430

    change

--- server/app.js
+++ server/app.js
@@ -9,13 +9,23 @@ app.listen(3000);
 // * Get 2 argument => path // calback function
 
 app.get("/", (req, res) => {
-
-  res.send("<p>home page</p>");
-
+  // res.send("<p>home page</p>");
+  res.sendFile("./views/index.html", { root: __dirname });
 });
 
 app.get("/about", (req, res) => {
+  // res.send("<p>about page</p>");
+  res.sendFile("./views/about.html", { root: __dirname });
+});
 
-  res.send("<p>about page</p>");
+// * Redirects
 
+app.get("/about-us", (req, res) => {
+  res.redirect("/about");
+});
+
+// * 404 page
+
+app.use((req, res) => {
+  res.status(404).sendFile("./views/404.html", { root: __dirname });
 });
